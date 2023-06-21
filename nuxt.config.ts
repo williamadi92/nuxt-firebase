5bc9734b34290d5ae880d6f3b6625544cc0034b3
William Adi Wibowo <williamadiwibowo@gmail.com>
2023-06-21 18:48:48 +0700

    chore: add env example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,14 @@ export default defineNuxtConfig({
   modules: [["nuxt-vuefire", {
     auth: true,
     config: {
-      apiKey: "AIzaSyCqArMa1bl73iGzAU5MtRAcNTL0F6DMnEE",
-      authDomain: "privy-aksel-fe-2.firebaseapp.com",
-      databaseURL: "https://privy-aksel-fe-2-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "privy-aksel-fe-2",
-      storageBucket: "privy-aksel-fe-2.appspot.com",
-      messagingSenderId: "961043557287",
-      appId: "1:961043557287:web:eb7e5351202c021db22482",
-      measurementId: "G-GEHC9EYN30"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID
     },
     admin: {
       serviceAccount: new URL('./service-account.json', import.meta.url)
